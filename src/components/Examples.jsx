8c64faee55ec7d1c5c20f61addd0db365efadb0a
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-08 15:11:43 +0200

    Componented

---
+++ src/components/Examples.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { EXAMPLES } from '../data.js';
+import TabButton from "./TabButton.jsx"
+import Section from './Section.jsx';
+
+export default function Examples() {
+
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    let tabContent = <p>Please Select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+                <code>
+                    {EXAMPLES[selectedTopic].code}
+                </code>
+            </pre>
+        </div>
+    }
+
+    const handleClick = (selectedButton) => {
+        // selectedButton => 'components', 'jsx', 'props', 'state'
+        setSelectedTopic(selectedButton);
+        // console.log(selectedButton);
+        // console.log(selectedTopic);
+    };
+
+    // console.log("App Component executing");
+
+    return (
+        <Section title='Examples' id="examples">
+            <menu>
+                {/* <TabButton>Component</TabButton> */}
+                <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleClick('components')} label="Component" />
+                <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleClick('jsx')} label="JSX" />
+                <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleClick('props')} label="Props" />
+                <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleClick('state')} label="State" />
+            </menu>
+            {tabContent}
+        </Section>
+    )
+}
